9312d1a8cc4529c823bc2341ce217dc27b24f5c9
DonySCode <donycastro80@gmail.com>
2024-08-03 23:43:25 -0400

    fix: fixed imports

--- src/navigation/MainNavigator.tsx
+++ src/navigation/MainNavigator.tsx
@@ -3,7 +3,8 @@ import React from 'react';
 import HomeScreen from '../screens/general/HomeScreen';
 import incidencesRegister from '../screens/incidences/incidences.register.tsx';
 import incidencesList from '../screens/incidences/incidences.list.tsx';
-import incidencesDetails from "../screens/incidences/incidences.details.tsx";
+import incidencesDetails from '../screens/incidences/incidences.details.tsx';
+import CustomDrawerContent from '../features/general/CustomDrawerContent.tsx';
 
 export type DrawerStackParamList = {
   Home: undefined;
@@ -14,7 +15,7 @@ const Drawer = createDrawerNavigator<DrawerStackParamList>();
 const MainNavigator = (): JSX.Element => {
   return (
     <>
-      <Drawer.Navigator initialRouteName="Home">
+      <Drawer.Navigator initialRouteName="Home" drawerContent={props => <CustomDrawerContent {...props} />}>
         <Drawer.Screen name="Home" component={HomeScreen} />
         <Drawer.Screen name="Registro de Incidencias" component={incidencesRegister} />
         <Drawer.Screen name="Lista de Incidencias" component={incidencesList} />
