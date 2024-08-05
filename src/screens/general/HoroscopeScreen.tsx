843afeed96efd8c1f69195e63f0d3bbf54199e9f
Vladimir Zapata <vzapata311@gmail.com>
2024-08-04 22:04:59 -0400

    styling horoscopo

--- src/screens/general/HoroscopeScreen.tsx
+++ src/screens/general/HoroscopeScreen.tsx
@@ -1,42 +1,45 @@
-import React, { useState } from "react";
-import { View, Text, Button, StyleSheet, ActivityIndicator } from "react-native";
-import { Picker } from "@react-native-picker/picker";
-import HoroscopeData from "../../features/general/HoroscopeData.ts";
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, ActivityIndicator} from 'react-native';
+import {Picker} from '@react-native-picker/picker';
+import HoroscopeData from '../../features/general/HoroscopeData.ts';
+import MinerdAppButton from '../../ui/MinerdAppButton.tsx';
 
 const zodiacSigns = [
-  "aries",
-  "taurus",
-  "gemini",
-  "cancer",
-  "leo",
-  "virgo",
-  "libra",
-  "scorpio",
-  "sagittarius",
-  "capricorn",
-  "aquarius",
-  "pisces"
+  'aries',
+  'taurus',
+  'gemini',
+  'cancer',
+  'leo',
+  'virgo',
+  'libra',
+  'scorpio',
+  'sagittarius',
+  'capricorn',
+  'aquarius',
+  'pisces',
 ] as const;
 
 type ZodiacSign = (typeof zodiacSigns)[number];
 
 const HoroscopeScreen: React.FC = () => {
-  const [selectedSign, setSelectedSign] = useState<ZodiacSign>("aries");
-  const [horoscope, setHoroscope] = useState<string>("");
+  const [selectedSign, setSelectedSign] = useState<ZodiacSign>('aries');
+  const [horoscope, setHoroscope] = useState<string>('');
   const [loading, setLoading] = useState<boolean>(false);
 
   const getHoroscope = async () => {
     setLoading(true);
     try {
       fetch(
-        `https://horoscope-app-api.vercel.app/api/v1/get-horoscope/daily?sign=${selectedSign}&day=TODAY`
-      ).then(response => response.json().then((r) => {
-        const data: HoroscopeData = r.data;
-        setHoroscope(data.horoscope_data);
-      }));
+        `https://horoscope-app-api.vercel.app/api/v1/get-horoscope/daily?sign=${selectedSign}&day=TODAY`,
+      ).then(response =>
+        response.json().then(r => {
+          const data: HoroscopeData = r.data;
+          setHoroscope(data.horoscope_data);
+        }),
+      );
     } catch (error) {
-      console.error("Error fetching horoscope:", error);
-      setHoroscope("Error fetching horoscope. Please try again later.");
+      console.error('Error fetching horoscope:', error);
+      setHoroscope('Error fetching horoscope. Please try again later.');
     } finally {
       setLoading(false);
     }
@@ -44,7 +47,6 @@ const HoroscopeScreen: React.FC = () => {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Horoscopo</Text>
       <Picker
         selectedValue={selectedSign}
         onValueChange={(itemValue: ZodiacSign) => setSelectedSign(itemValue)}
@@ -53,9 +55,9 @@ const HoroscopeScreen: React.FC = () => {
           <Picker.Item key={sign} label={sign} value={sign} />
         ))}
       </Picker>
-      <Button title="Get Horoscope" onPress={getHoroscope} />
+      <MinerdAppButton title="Obtener horóscopo" onPress={getHoroscope} />
       {loading && <ActivityIndicator size="large" style={styles.loading} />}
-      {horoscope !== "" && <Text style={styles.horoscope}>{horoscope}</Text>}
+      {horoscope !== '' && <Text style={styles.horoscope}>{horoscope}</Text>}
     </View>
   );
 };
@@ -64,25 +66,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
-    justifyContent: "center"
   },
   title: {
     fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 20
+    fontWeight: 'bold',
+    marginBottom: 20,
   },
   picker: {
     height: 50,
-    width: "100%",
-    marginBottom: 10
+    width: '100%',
+    marginBottom: 10,
   },
   loading: {
-    marginTop: 20
+    marginTop: 20,
   },
   horoscope: {
     fontSize: 18,
-    marginTop: 20
-  }
+    marginTop: 20,
+  },
 });
 
 export default HoroscopeScreen;
